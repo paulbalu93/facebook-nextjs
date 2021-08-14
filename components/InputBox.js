41fc5299d6210b6832e95a4bdd8d5a6a87b41c90
Paul Maliakel <paulbalu93@gmail.com>
2021-08-15 00:26:36 +0200

    adds firebase image uploading

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,11 +1,74 @@
 import { useSession } from "next-auth/client";
 import Image from "next/image";
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { useRef, useState } from "react";
+import { db, storage } from "../firebase";
+import firebase from "firebase";
 
 function InputBox() {
   const [session] = useSession();
+  const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    if (!inputRef.current.value) return;
+
+    db.collection("posts")
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamps: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, "data_url");
+          removeImageFromPost();
+          uploadTask.on(
+            "state_change",
+            null,
+            (error) => console.error(error),
+            () => {
+              storage
+                .ref("posts")
+                .child(doc.id)
+                .getDownloadURL()
+                .then(
+                  (url) => {
+                    db.collection("posts").doc(doc.id).set({ postImage: url });
+                  },
+                  { merge: true }
+                );
+            }
+          );
+        }
+      });
+
+    inputRef.current.value = "";
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+  console.log(imageToPost);
+  const removeImageFromPost = () => {
+    setImageToPost(null);
+  };
   return (
-    <div>
-      <div>
+    <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+      <div className="flex items-center space-x-4 ">
         <Image
           src={session.user.image}
           className="rounded-full"
@@ -13,7 +76,49 @@ function InputBox() {
           height={40}
           layout="fixed"
         />
-        <form className="flex flex-1"></form>
+        <form className="flex flex-1">
+          <input
+            type="text"
+            ref={inputRef}
+            className="rounded-full h-12 bg-gray-100 flex-grow px-5 outline-none"
+            placeholder={`whats on your mind, ${session.user.name} ?`}
+          />
+          <button hidden type="submit" onClick={sendPost}>
+            Submit
+          </button>
+        </form>
+        {imageToPost && (
+          <div
+            onClick={removeImageFromPost}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <img className="h-10 object-contain" src={imageToPost} alt="" />
+            <p className="text-xs text-red-500 text-center"> Remove</p>
+          </div>
+        )}
+      </div>
+      <div className="flex justify-evenly p-3 border-t mt-5">
+        <div className="inputIcon">
+          <VideoCameraIcon className="h-7 text-red-500 " />
+          <p className="text-xs sm:text-sm xl:text-base"> Live Video</p>
+        </div>
+        <div
+          onClick={() => filePickerRef.current.click()}
+          className="inputIcon"
+        >
+          <CameraIcon className="h-7 text-green-300" />
+          <p className="text-xs sm:text-sm xl:text-base"> Photo/Video</p>
+          <input
+            ref={filePickerRef}
+            type="file"
+            onChange={addImageToPost}
+            hidden
+          />
+        </div>
+        <div className="inputIcon">
+          <EmojiHappyIcon className="h-7 text-yellow-300" />
+          <p className="text-xs sm:text-sm xl:text-base"> Feeling/Activity</p>
+        </div>
       </div>
     </div>
   );
--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 import firebase from "firebase";
+import "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCfkO-ETtCrYRAoe8wkLLmA8HbCXjZ42Ao",
   authDomain: "facebook-clone-9be29.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
   messagingSenderId: "218115947405",
   appId: "1:218115947405:web:b50097fe23136270319bc4",
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const storage = firebase.storage();
+
+export { db, storage };
